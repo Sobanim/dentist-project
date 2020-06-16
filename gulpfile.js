a3a8c3da86911026396f585f9d582985748d0795
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2020-06-16 21:46:11 +0200

    added browsersyn. Configure gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,44 @@ let source_folder = "src";
 let path={
     build:{
         html: project_folder + "/",
-        css: project_folder + "/css/,"
+        css: project_folder + "/css/",
         js: project_folder + "/js/",
         img: project_folder + "/img/"
         // fonts: project_folder + "/fonts/",
     },
     src:{
         html: source_folder + "/",
-        css: source_folder + "/css/,"
-        js: source_folder + "/js/",
-        img: source_folder + "/img/"
-        // fonts: source_folder + "/fonts/",
-    }
+        css: source_folder + "/css/style.css",
+        // css: source_folder + "/scss/style.css",
+        js: source_folder + "/js/script.js",
+        img: source_folder + "/img/**/*.{jpg, png, svg, gif, ico, webp}"
+        // fonts: source_folder + "/fonts/*.ttf",
+    },
+    watch:{
+        html: source_folder + "/**/*.html",
+        css: source_folder + "/css/**/*.css",
+        // css: source_folder + "/scss/style.css",
+        js: source_folder + "/js/**/*.js",
+        img: source_folder + "/img/**/*.{jpg, png, svg, gif, ico, webp}"
+    },
+    clean: "./" + project_folder + "/"
 }
+
+let {src, dest} = require('gulp'),
+    gulp = require('gulp'),
+    browsersync = require("browser-sync").create();
+
+    function browserSync(params){
+        browsersync.init({
+            server:{
+                baseDir:"./" + project_folder + "/"
+            },
+            port: 3000,
+            notify: false
+        })
+    }
+
+let watch = gulp.parallel(browserSync);
+
+exports.watch = watch;
+exports.default = watch;
