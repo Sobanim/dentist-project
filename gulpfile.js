f8d9b440963136a1ad15dac4d561b20dbaa0d2da
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2020-06-23 17:09:30 +0200

    Some changes for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ let path = {
         html: [source_folder + "/*.html", "!"+ source_folder + "/_*.html"],
         css: source_folder + "/scss/style.scss",
         js: source_folder + "/js/script.js",
-        img: source_folder + "/img/**/*.{jpg, png, svg, gif, ico, webp}"
+        img: source_folder + "/img/*.{jpg, png, svg, gif, ico, webp}"
         // fonts: source_folder + "/fonts/*.ttf"
     },
     watch: {
@@ -35,7 +35,9 @@ let {src, dest} = require('gulp'),
     autoprefixer = require("gulp-autoprefixer"),
     group_media = require("gulp-group-css-media-queries"),
     clean_css = require("gulp-clean-css"),
-    rename = require("gulp-rename");
+    rename = require("gulp-rename"),
+    uglify = require("gulp-uglify-es").default,
+    imageMin = require("gulp-imagemin");
 
 
     function browserSync(params){
@@ -81,20 +83,54 @@ function css(){
         .pipe(browsersync.stream())
 }
 
+function js(){
+    return src(path.src.js)
+        .pipe(fileinclude())
+        .pipe(dest(path.build.js))
+        .pipe(
+            uglify()
+        )
+        .pipe(
+            rename({
+                extname: ".min.js"
+            })
+        )
+        .pipe(dest(path.build.js))
+        .pipe(browsersync.stream())
+}
+
+function images(){
+    return src(path.src.img)
+        .pipe(
+            imageMin({
+                progressive: true,
+                svgoPlugins: [{ removeViewBox: false }],
+                interfaced: true,
+                optimizationLevel: 3// 0 to 7
+            })
+        )
+        .pipe(dest(path.build.img))
+        .pipe(browsersync.stream())
+}
+
 function watchFiles(params){
     gulp.watch([path.watch.html], html)
     gulp.watch([path.watch.css], css)
+    gulp.watch([path.watch.js], js)
+    gulp.watch([path.watch.img], images)
 }
 
 function clean (params) {
     return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(css, html));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
 exports.html = html;
 exports.css = css;
+exports.js = js;
+exports.images = images;
 exports.build = build;
 exports.watch = watch;
 exports.default = watch;
